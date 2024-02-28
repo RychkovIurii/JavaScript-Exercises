b6967059a150aad16140f8b8fb4355b0e9b3cd86
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-02-28 13:41:49 +0200

    add js file and moved the script from html to js

---
+++ ex00/script.js
@@ -0,0 +1,9 @@
+var num = 2;
+console.log("message console.log");
+console.log("message second console.log");
+window.alert("message window.alert");
+alert("message alert");
+document.write("message document write");
+document.write("message second document write<br>");
+document.write("message third document write");
+var info = document.getElementById("info").innerHTML="message innerHTML";
